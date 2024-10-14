edead3aa54f06df080992ab77d1c2f5f7cf42a3e
shahr <shahrzado@hotmail.com>
2024-10-14 23:05:15 +0100

    Add project files.

---
+++ HA_CheckoutTechTaskTests/CheckoutTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HA_CheckoutTechTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HA_CheckoutTechTask.Tests
+{
+	[TestClass()]
+	public class CheckoutTests
+	{
+		[TestMethod()]
+		public void ScanTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetTotalPriceTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
